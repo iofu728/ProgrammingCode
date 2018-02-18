5da7262f413d1ed9cfd63448ff8fb946a3b3fe92
iofu728 <iofu728@163.com>
2018-02-18 22:34:39 +0800

    1001-1005

---
+++ 已完成教程/PAT A 1005.cpp
@@ -0,0 +1,32 @@
+#include<cstdio>
+#include<vector>
+#include<cstring>
+char num[10][10]={"zero","one","two","three","four","five","six","seven","eight","night"};
+
+
+void DFS(int n){
+	if(n/10==0){
+		printf("%s",num[n%10]);
+		return;
+	}
+	DFS(n/10);
+	printf(" %s",num[n%10]);
+	
+}
+int main(){
+	
+	char n[111];
+	int a,sum=0;
+	gets(n);
+	int len=strlen(n);
+	for(int i=0;i<len;i++){
+		a=(n[i]-'0');
+		sum+=a;
+	}
+	
+	DFS(sum);
+	
+	
+	return 0;
+}
+

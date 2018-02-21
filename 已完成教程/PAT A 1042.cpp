2809d5f0dd1c15c3ac2fd206bbd2c3cb61f6986b
iofu728 <iofu728@163.com>
2018-02-21 23:25:09 +0800

    1042

---
+++ 已完成教程/PAT A 1042.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    getchar();
+    int start[55], end[55], scan[55];
+    for(int i = 1; i < 55; i++) {
+        cin>>scan[i];
+        end[i] = i;
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 1; j < 55; j++)
+            start[j] = end[j];
+        for(int k = 1; k < 55; k++)
+            end[scan[k]] = start[k];
+    }
+    char c[6] = {"SHCDJ"};
+    for(int i = 1; i < 55; i++) {
+        end[i] = end[i] - 1;
+        printf("%c%d", c[end[i]/13], end[i]%13+1);
+        if(i != 54) printf(" ");
+    }
+    return 0;
+}
